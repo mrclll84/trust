9c4c685daad49e7cda3cf7a7f960c44e9cad67e6
-Miracle <73531883+Merck-web@users.noreply.github.com>
2024-08-20 00:21:57 +0500

    Анимация

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const menues = Array.from(document.querySelectorAll('.nav-list__menu'));
+const headerMenu = document.querySelector('.header-menu');
+const allHide = document.querySelector('.all-hide');
+
+for (const menu of menues) {
+    menu.addEventListener('mouseout', event => {
+        console.log(event, 'mouseout')
+        headerMenu.classList.remove('open')
+        allHide.classList.remove('open')
+    })
+    menu.addEventListener('mouseover', event => {
+        console.log(event, 'mouseover')
+        headerMenu.classList.add('open')
+        allHide.classList.add('open')
+    })
+}
