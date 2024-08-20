5ae339b94db89534963d94651389268a2a7b9660
-Miracle <73531883+Merck-web@users.noreply.github.com>
2024-08-20 21:26:22 +0500

    мобильное меню

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 const menues = Array.from(document.querySelectorAll('.nav-list__menu')) || [];
 const headerMenu = document.querySelector('.header-menu');
 const allHide = document.querySelector('.all-hide');
+const hamburger = document.querySelector('.hamburger');
+const miniMenu = document.querySelector('.mini-header');
 const miniMenues = Array.from(document.querySelectorAll('.header-menu__content')) || []
 
 for (const menu of menues) {
@@ -27,11 +29,6 @@ headerMenu.addEventListener('mouseover', event => {
 })
 
 
-headerMenu.classList.add('open')
-allHide.classList.add('open')
-const el  = document.querySelector(`.header-menu__content[data-id="${1}"]`)
-el.classList.add('show')
-
 window.addEventListener('mouseover', event => {
 
     if (!event.target.closest('.header')) {
@@ -42,3 +39,16 @@ window.addEventListener('mouseover', event => {
         allHide.classList.remove('open')
     }
 })
+
+hamburger.addEventListener('click', event => {
+    hamburger.classList.toggle('is-active')
+    miniMenu.classList.toggle('is-active')
+
+    if (hamburger.classList.contains('is-active')) {
+        document.querySelector('body').style.overflowY = 'hidden'
+    }
+    else {
+        document.querySelector('body').style.overflowY = ''
+    }
+})
+
