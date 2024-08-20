454f26a2f336bcb3bbeaa7bcaaa6f0cbdcfa13c7
-Miracle <73531883+Merck-web@users.noreply.github.com>
2024-08-20 20:41:27 +0500

    меню сверху

--- js/index.js
+++ js/index.js
@@ -1,17 +1,23 @@
-const menues = Array.from(document.querySelectorAll('.nav-list__menu'));
+const menues = Array.from(document.querySelectorAll('.nav-list__menu')) || [];
 const headerMenu = document.querySelector('.header-menu');
 const allHide = document.querySelector('.all-hide');
+const miniMenues = Array.from(document.querySelectorAll('.header-menu__content')) || []
 
 for (const menu of menues) {
-    menu.addEventListener('mouseout', event => {
-        console.log(event, 'mouseout')
-        headerMenu.classList.remove('open')
-        allHide.classList.remove('open')
-    })
     menu.addEventListener('mouseover', event => {
-        console.log(event, 'mouseover')
+        const id = event.target.dataset.id;
+
         headerMenu.classList.add('open')
         allHide.classList.add('open')
+
+        const el  = document.querySelector(`.header-menu__content[data-id="${id}"]`)
+
+        if (el) {
+            for (const m of miniMenues) {
+                m.classList.remove('show')
+            }
+            el.classList.add('show')
+        }
     })
 }
 
@@ -20,7 +26,19 @@ headerMenu.addEventListener('mouseover', event => {
     allHide.classList.add('open')
 })
 
-headerMenu.addEventListener('mouseout', event => {
-    headerMenu.classList.remove('open')
-    allHide.classList.remove('open')
+
+headerMenu.classList.add('open')
+allHide.classList.add('open')
+const el  = document.querySelector(`.header-menu__content[data-id="${1}"]`)
+el.classList.add('show')
+
+window.addEventListener('mouseover', event => {
+
+    if (!event.target.closest('.header')) {
+        for (const m of miniMenues) {
+            m.classList.remove('show')
+        }
+        headerMenu.classList.remove('open')
+        allHide.classList.remove('open')
+    }
 })
